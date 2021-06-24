9cea402525f3eef3400c5fbe5c1f018d312857f7
Giao Ho <joutvhu@gmail.com>
2021-06-24 08:58:21 +0700

    Parse second

--- src/main/java/com/joutvhu/date/parser/strategy/SecondStrategy.java
+++ src/main/java/com/joutvhu/date/parser/strategy/SecondStrategy.java
@@ -2,6 +2,10 @@ package com.joutvhu.date.parser.strategy;
 
 import com.joutvhu.date.parser.domain.DateStorage;
 import com.joutvhu.date.parser.domain.StringSource;
+import com.joutvhu.date.parser.exception.MismatchException;
+import com.joutvhu.date.parser.util.CommonUtil;
+
+import java.util.Iterator;
 
 public class SecondStrategy extends Strategy {
     public SecondStrategy(char c) {
@@ -15,6 +19,30 @@ public class SecondStrategy extends Strategy {
 
     @Override
     public void parse(DateStorage storage, StringSource source, NextStrategy chain) {
-        this.nextStrategy(chain);
+        StringSource.PositionBackup backup = source.backup();
+        Iterator<String> iterator = source.iterator(this.pattern.length(), 2);
+
+        while (iterator.hasNext()) {
+            String value = iterator.next();
+            if (CommonUtil.isNumber(value)) {
+                try {
+                    this.nextStrategy(chain);
+                } catch (MismatchException e) {
+                    if (!iterator.hasNext()) {
+                        backup.restore();
+                        throw e;
+                    }
+                }
+
+                int second = Integer.parseInt(value);
+                if (second < 0 || second > 59)
+                    throw new MismatchException("The \"" + value + "\" is not a second.", backup.getBackup(), this.pattern);
+                storage.setSecond(second);
+                return;
+            } else {
+                backup.restore();
+                throw new MismatchException("The \"" + value + "\" is not a second.", backup.getBackup(), this.pattern);
+            }
+        }
     }
 }
